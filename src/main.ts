7b7efddb96b71812c8143030bb359b0719205cfd
fahnal <fahnaladitia07@gmail.com>
2022-08-29 20:11:09 +0800

    improve helmet and cors

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,19 @@ import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
-import { ValidationPipe } from './v1/pipe/validation.pipe';
+import helmet from 'helmet';
+import { ValidationPipe } from './v1/pipe';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix('api');
   const config = app.get(ConfigService);
   const PORT = config.get('PORT');
+
+  app.use(helmet());
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
 
   await app.listen(PORT).then(() => {
     logger.log(`server running at http://localhost:${PORT}`);
